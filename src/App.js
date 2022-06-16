f029f70c0c8804a5d4c22a90efff600a5b54cd77
cushonz <cushonz@cwu.edu>
2022-06-16 01:08:05 -0700

    React select drop down implemented

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import logo from './Assets/pokeball.svg';
 import './Styling/App.css';
 import Grid from './Components/grid'
-import {Dropdown} from './Components/dropdown'
-import {Element} from "./Components/element";
+import Select from "react-select/base";
+import {useState} from "react";
 
 
-
+const options = [
+     {value : 'Chocolate', label: 'Chocolate'},
+     {value : 'vannila', label: 'vanilla'}
+ ]
 function App() {
+    const [setSelected] = useState(0);
+    const handleChange = e => {
+        setSelected(e.value);
+    }
   return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Grid />
-            <Dropdown>
-                <Element/>
-            </Dropdown>
 
+            <form>
+                <Select
+                    options ={options}
+                    placeholder="Generation"
+                    onChange={handleChange}
+                />
+            </form>
 
 
             <p>
